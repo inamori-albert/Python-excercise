d62d1c10a9b3869598b739942ad095ddb983642f
toru inamori <toru_inamori@albert2005.co.jp>
2017-08-10 15:49:52 +0900

    とりあえず動くテストケースとテスト対象を作成。処理はなし。

--- tdd_sample/fizzbuzz.py
+++ tdd_sample/fizzbuzz.py
@@ -1,1 +1,7 @@
 
+# fizzbuzz仕様
+# * numを渡すと1～numまでのfizzbuzzの入ったリストを返す
+
+def FizzBuzz(num):
+    print("FizzBuzz run.")
+    return []
--- tdd_sample/test_fizzbuzz.py
+++ tdd_sample/test_fizzbuzz.py
@@ -1,3 +1,28 @@
 import unittest
+from fizzbuzz import FizzBuzz #テスト対象
 
 
+class TestFizzBuzz(unittest.TestCase):
+
+
+    def setUp(self):
+        # 初期化処理
+        pass
+
+
+    def tearDown(self):
+        # 終了処理
+        pass
+
+
+    def testNewClass(self):
+        # テストを書いていく
+        self.assertNotEqual(FizzBuzz(10), None)
+
+
+class TestRandom(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
