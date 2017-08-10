85421f20b97ddd90103463d1b819c1eee61f7e03
toru inamori <toru_inamori@albert2005.co.jp>
2017-08-10 13:06:25 +0900

    テスト前処理と後処理の追加

--- unittest/test1.py
+++ unittest/test1.py
@@ -4,13 +4,28 @@ import unittest
 #  python -m unittest test.py でテストできる
 class MyTest(unittest.TestCase):
 
+    # 各テスト前に呼び出される
+    # DBのセットアップやテストデータの準備を行う
+    def setUp(self):
+        print("SetUp is called.")
+
+    # 各テスト後に呼び出される
+    # テストの後処理(成果物の削除等)やDB処理の後処理などを行う
+    def tearDown(self):
+        print("tearDown is called.")
+
+    # テストメソッド
     # メソッド名のプレフィックスにtestをつける
-    def test_do_somethig(self):
+    def test_do_somethig1(self):
         # テストケースを書く
         one = 1
         self.assertEqual(one, 1, "one is 1")
 
+    def test_do_somethig2(self):
+        # テストケースを書く
+        two = 2
+        self.assertEqual(two, 2, "two is 2")
+
 
 if __name__ == "__main__":
     unittest.main()
-    
